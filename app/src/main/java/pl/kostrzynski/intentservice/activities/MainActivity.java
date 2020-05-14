166ba3cde33335933a6d6c835e2ea900fa93bb24
RafalKostrzynski <rafx97@gmail.com>
2020-05-14 18:13:05 +0200

    Started Service and connection to an external API

---
+++ app/src/main/java/pl/kostrzynski/intentservice/activities/MainActivity.java
@@ -0,0 +1,29 @@
+package pl.kostrzynski.intentservice.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import pl.kostrzynski.intentservice.R;
+import pl.kostrzynski.intentservice.service.ChuckNorrisIntentService;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickChuckServiceStart(View view){
+        Intent intent=new Intent(this,ChuckNorrisIntentService.class);
+        startService(intent);
+    }
+
+    public void onClickChuckServiceStop(View view){
+        Intent intent=new Intent(this,ChuckNorrisIntentService.class);
+        stopService(intent);
+    }
+
+}
